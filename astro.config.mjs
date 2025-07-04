789efa6b579d985a54ef7f451d9deb80c3f4ad92
Pretti_ <33943962+xPretti@users.noreply.github.com>
2025-07-03 23:59:54 -0300

    feat: dad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,64 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import mdx from "@astrojs/mdx";
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			favicon: '/public/favicon.ico',
-			title: 'ScriptBot',
-			logo: {
-				src: './src/assets/Botrading.png',
-			},
-			social: [{
-				icon: 'discord',
-				label: 'discord',
-				href: 'https://discord.botrading.net'
-			}],
-			sidebar: [
-				{
-					label: 'Indicators',
-					autogenerate: { directory: 'indicators' },
-				},
-				{
-					label: 'Placeholders',
-					autogenerate: { directory: 'placeholders' },
-				},
-				{
-					label: 'Moldable Vars',
-					autogenerate: { directory: 'moldablevars' },
-				},
-			],
-
-			customCss: [
-				'./src/styles/custom.css',
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      favicon: "/public/favicon.ico",
+      title: "ScriptBot",
+      logo: {
+        src: "./src/assets/Botrading.png",
+      },
+      social: [
+        {
+          icon: "discord",
+          label: "discord",
+          href: "https://discord.botrading.net",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Indicators",
+          autogenerate: { directory: "indicators" },
+        },
+        {
+          label: "Constants",
+          autogenerate: { directory: "constants" },
+        },
+        {
+          label: "Variables",
+          autogenerate: { directory: "variables" },
+        },
+        {
+          label: "Functions",
+          items: [
+            "functions/functions",
+            {
+              label: "Types",
+              // collapsed: true,
+              autogenerate: { directory: "functions/types" },
+            },
+          ],
+        },
+        {
+          label: "Moldable Vars",
+          autogenerate: { directory: "moldablevars" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+    mdx(),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        "@components": path.resolve("./src/components"),
+      },
+    },
+  },
 });
