9c9ed28965d24c2d090c175c5533e44877a5e0f1
Pretti_ <33943962+xPretti@users.noreply.github.com>
2026-09-12 01:43:58 -0300

    feat: adicionado traduções

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = import.meta.env.BASE_URL;
+export const DEFAULT_LOCALE = "pt-BR";
--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,8 +1,13 @@
-import { BASE_URL } from "@src/configs/config";
+import { BASE_URL, DEFAULT_LOCALE } from "@src/configs/config";
 
 export function getUrl(path: string) {
    if (BASE_URL === "/") {
       return path;
    }
    return `${BASE_URL}${path}`;
+}
+
+export function getAbsolute(currentLocale: string, path: string) {
+   if (currentLocale === DEFAULT_LOCALE) return getUrl(path);
+   return getUrl(`/${currentLocale}${path}`);
 }
